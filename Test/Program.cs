1383dc6d539be80f8f4b5d3fc3969197e12c68b0
Alexandre Golfier <62390407+Alexmister63@users.noreply.github.com>
2021-02-03 11:04:25 +0100

    Add files via upload

---
+++ Test/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using Modele;
+using Data;
+using static System.Console;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace Test
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            ListPerso pTest = Stub.CreationDePersonnages();
+            pTest.ToString();
+        }
+    }
+}
